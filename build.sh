663207ce8be3919ed75e8a6b5d04477211becc76
Adelina Tuvenie <adelinatuvenie@gmail.com>
2020-07-01 16:54:44 +0200

    Add trap to e2e build script

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ apt-get update && apt-get install rsync -y
 K8S_REPO=http://github.com/kubernetes/kubernetes
 K8S_SRC_PATH=${K8S_SRC_PATH:-/go/src/k8s.io/kubernetes}
 
-if git clone ${K8S_REPO} ${K8S_SRC_PATH}; then
-       echo "Error cloning k8s repo."
+trap "rm -f ${K8S_SRC_PATH}" EXIT
+
+git clone ${K8S_REPO} ${K8S_SRC_PATH}
 
 pushd ${K8S_SRC_PATH}
 
