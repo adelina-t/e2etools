d564ffe5be1288d0866b09c3842cb6d276229f79
Ubuntu <azureuser@k8s-master-42891911-0.nug02ufy5tjupkishb4ttytmie.cx.internal.cloudapp.net>
2020-07-01 15:25:57 +0000

    Minor fixup

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ apt-get update && apt-get install rsync -y
 K8S_REPO=http://github.com/kubernetes/kubernetes
 K8S_SRC_PATH=${K8S_SRC_PATH:-/go/src/k8s.io/kubernetes}
 
-trap "rm -f ${K8S_SRC_PATH}" EXIT
+trap "rm -f ${K8S_SRC_PATH}" ERR 
 
+rm -rf ${K8S_SRC_PATH}
 git clone ${K8S_REPO} ${K8S_SRC_PATH}
 
 pushd ${K8S_SRC_PATH}
